89be8261e9137ef03f3971d6a2d89cc33f24437e
edu-correa <eduardo.correa@sptech.school>
2023-05-13 22:32:37 -0300

    mudei o link do require

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -1,4 +1,4 @@
-var database = require("../database/config")
+var database = require("../database/connection")
 
 function listar() {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
