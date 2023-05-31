e60841b0d0dc4dfcac677bb97385fd151cc7de3c
edu-correa <eduardo.correa@sptech.school>
2023-05-30 22:51:30 -0300

    alterei o buscarTodos() e o listar1

--- src/models/ambienteModel.js
+++ src/models/ambienteModel.js
@@ -3,14 +3,13 @@ var database = require("../database/connection")
 function listar1(idAmbiente) {
     console.log("ACESSEI O AMBIENTE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar() \n\n " + idAmbiente);
     var instrucao = `
-        select SUM(valMetrica) as soma, HOUR(dateMetrica) as horario 
-        from tbMetricas
-        join tbSensor on fkSensor = idSensor
-        join tbAmbiente on fkAmbiente = idAmbiente
-        where idAmbiente = ${idAmbiente} and YEAR(dateMetrica) = YEAR(now()) and DAY(now()) = DAY(dateMetrica) and 
-        MONTH(dateMetrica) = MONTH(now())
-        GROUP BY HOUR(dateMetrica)
-        order by HOUR(dateMetrica) asc;
+    select SUM(valMetrica) as soma, HOUR(dateMetrica) as horario 
+    from tbMetricas
+    join tbSensor on fkSensor = idSensor
+    join tbAmbiente on fkAmbiente = idAmbiente
+    where idAmbiente =  ${idAmbiente} and DATEDIFF(dateMetrica, now()) < "23:30:00"
+    GROUP BY HOUR(dateMetrica)
+    order by HOUR(dateMetrica) asc;
 
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
@@ -50,14 +49,14 @@ function buscarAmbientes(idUsuario) {
 function buscarTodos(idUsuario) {
     console.log("ACESSEI O AMBIENTE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar() \n\n " + idUsuario);
     var instrucao = `
-    select SUM(valMetrica) as soma, minute(dateMetrica) as horario, idAmbiente, tbAmbiente.*
+    select SUM(valMetrica) as soma, minute(dateMetrica) as horario, idAmbiente, minimoPessoas, maximoPessoas
     from tbMetricas
     join tbSensor on fkSensor = idSensor
     join tbAmbiente on fkAmbiente = idAmbiente
     join tbEmpresa on tbAmbiente.fkEmpresa = idEmpresa
     JOIN tbUsuario ON idEmpresa = tbUsuario.fkEmpresa
-    where DATEDIFF(dateMetrica, now()) = "00:30:00" and idUsuario = ${idUsuario}
-    GROUP BY minute(dateMetrica)
+    where DATEDIFF(dateMetrica, now()) < "23:30:00" and idUsuario = ${idUsuario}
+    GROUP BY idAmbiente
     order by minute(dateMetrica);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
