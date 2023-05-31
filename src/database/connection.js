db7114aef78e7627f9ecbe6eed1e9914e95238d9
edu-correa <eduardo.correa@sptech.school>
2023-05-31 15:49:13 -0300

    alterei o mapa de calor

--- src/database/connection.js
+++ src/database/connection.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "bd_SMFP",
-    user: "root",
-    password: "",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
